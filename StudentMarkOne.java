90b03df0f24d29c897ab1f3572658f3b87c9c8ba
baju123 <32977824+baju123@users.noreply.github.com>
2017-10-21 05:50:49 -0400

    Add files via upload

---
+++ StudentMarkOne.java
@@ -0,0 +1,20 @@
+
+public class StudentMarkOne {
+public static void main(String[] args) {
+	int[][]studentMarks = new int [3][3];
+	
+	studentMarks [0][0] = 78;
+	studentMarks [0][1] = 65;
+	studentMarks [0][2] = 88;
+	
+	studentMarks [0][0] = 73;
+	studentMarks [0][1] = 34;
+	studentMarks [0][2] = 80;
+	
+	studentMarks [0][0] = 90;
+	studentMarks [0][1] = 98;
+	studentMarks [0][2] = 95;
+	
+	
+}
+}
